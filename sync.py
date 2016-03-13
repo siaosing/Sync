11701b8ab0c7b9b126532f37a47c7b095df96bd9
synack <garth.stone@atrumverum.com>
2016-03-13 12:08:09 +1100

    Variable names updated to be more generic

--- sync.py
+++ sync.py
@@ -4,12 +4,12 @@ import os
 import re
 import shutil
 
-tvFolderList = []
-tvFolderList2 = []
+tFolderList = []
+tFolderList2 = []
 deltaList = []
 
-tv2Path = "/Users/villan/Desktop/Python/Queue2/"
-tvPath = "/Users/villan/Desktop/Python/Queue/"
+t2Path = "/Users/villan/Desktop/Python/Queue2/"
+tPath = "/Users/villan/Desktop/Python/Queue/"
 pathFinder = r"(.*/)(.*)$"
 
 print("")
@@ -38,36 +38,35 @@ def folder_capture(path, switch):
 
 # TV Folder capture
 
-tvFolderList = folder_capture(tvPath, "Source")
+tFolderList = folder_capture(tPath, "Source")
 
 
 # TV2 folder capture
 
-tvFolderList2 = folder_capture(tv2Path, "Destination")
+tFolderList2 = folder_capture(t2Path, "Destination")
 				
 print("")
 
 # Compare folder capture
 
-for file in tvFolderList:
-	if not file in tvFolderList2:
+for file in tFolderList:
+	if not file in tFolderList2:
 		deltaList.append(file)
 
 if not deltaList:
 	print('\nDirectories are in sync.' + '\n')
 else:
-	print("Delta between folders: ")
-	print(deltaList)
+	print("Delta between folders: " + str(len(deltaList)))
 	print("")
 
 	for newFile in deltaList:
 		m = re.search(pathFinder, newFile)
 		if m:
-			newFolder = tv2Path + m.group(1) 
+			newFolder = t2Path + m.group(1) 
 			if not os.path.exists(newFolder):
 				os.makedirs(newFolder)
 			
-			new = tvPath + newFile
+			new = tPath + newFile
 			shutil.copy(new, newFolder)
 			print("Copying: " + newFile + " -> " + m.group(1))
 
